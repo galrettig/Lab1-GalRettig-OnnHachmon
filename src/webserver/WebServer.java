5ceba5df972ff71f24df044cba86a937cc9d3f82
gal <galrettig@gmail.com>
2015-12-04 19:05:13 +0200

    yoyo

---
+++ src/webserver/WebServer.java
@@ -0,0 +1,36 @@
+package webserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class WebServer {
+	
+	public static void Main(String[] args) throws IOException{
+		
+		
+	    ServerSocket serverSoc;
+		
+			serverSoc = new ServerSocket(8080);
+			 while(true) {
+			    	
+			    	Socket connection = serverSoc.accept();
+			    	
+			    	
+			    	
+			    	
+			    	
+			    	
+			    	
+
+			}
+		
+			
+	   
+	   
+		
+	}
+
+}
