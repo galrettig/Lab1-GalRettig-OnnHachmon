25ac214ebf7c678682be70ae430e86e82df3acd6
onncho <onncho@Onns-MacBook-Pro.local>
2015-12-05 19:15:02 +0200

    cons class and fix web server

--- src/webserver/WebServer.java
+++ src/webserver/WebServer.java
@@ -15,23 +15,23 @@ public class WebServer {
 		try {
 			serverSoc = new ServerSocket(8080);
 			while(true) {
-
+				
 				Socket connection = serverSoc.accept();
 				String fullRequest = "";
-
-				BufferedReader inputClient = new BufferedReader(new InputStreamReader(connection.getInputStream( ) ) );
-
-
-				while((clientInput = inputClient.readLine()) != null){
-					fullRequest += clientInput;
+				BufferedReader inputClient = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				String line = inputClient.readLine();
+				while(line != null && !line.equals(""))
+				{
+					System.out.println(line);
+					fullRequest += line;
+					line = inputClient.readLine();
 				}
-				System.out.println(fullRequest);
-
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-
 }
