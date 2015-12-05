e8a51adaa270c5fa0e7a0209b76f9272d6b45eb8
onncho <onncho@Onns-MacBook-Pro.local>
2015-12-05 21:06:00 +0200

    regex

--- src/webserver/ConfigurationObject.java
+++ src/webserver/ConfigurationObject.java
@@ -1,13 +1,13 @@
 package webserver;
 
 public class ConfigurationObject {
-	
-	int m_Port;
+
+	String m_Port;
 	String m_rootFolder;
 	String m_defualtPage;
-	int m_MaxThreads;
-	
-	public ConfigurationObject(String i_StringFromConfFile) {
-		
+	String m_MaxThreads;
+
+	public ConfigurationObject() {
+
 	}
 }
--- src/webserver/WebServer.java
+++ src/webserver/WebServer.java
@@ -7,6 +7,8 @@ import java.io.InputStreamReader;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WebServer {
 
@@ -20,17 +22,49 @@ public class WebServer {
 	public static void readFile()
 	{
 		Path file = FileSystems.getDefault().getPath("/Users/onncho/Dropbox/Study/IDC/Semester 5/תקשורת/lab1/Lab1-GalRettig-OnnHachmon/configuration", "myWebConf.ini");
-		
+		StringBuilder fileData = new StringBuilder("");
 		try (InputStream in = Files.newInputStream(file);
 		    BufferedReader reader =
 		      new BufferedReader(new InputStreamReader(in))) {
 		    String line = null;
 		    while ((line = reader.readLine()) != null) {
-		        System.out.println(line);
+		        //System.out.println(line);
+		    	fileData.append("\n");
+		        fileData.append(line);
 		    }
 		} catch (IOException e) {
 		    System.err.println(e);
 		}
+		
+		fillConfigurationObject(fileData);
 
+	}
+
+
+	private static void fillConfigurationObject(StringBuilder i_StringFromConfFile) {
+		// TODO Auto-generated method stub
+		System.out.println(i_StringFromConfFile);
+		String portRegex = "port=[' ']*([0-9]+)";
+		String rootRegex = "root=[' ']*(.*)";
+		String defaultPageRegex = "defaultPage=[' ']*(.*)";
+		String maxThreadsRegex = "maxThreads=[' ']*(.*)";
+		
+		// Construct Conf Object
+		ConfigurationObject myConfObject = new ConfigurationObject();
+		
+		// Create a Pattern object
+	    Pattern patternObject = Pattern.compile(portRegex);
+	    
+	    // Now create matcher object.
+	    Matcher matcherObject = patternObject.matcher(i_StringFromConfFile);
+		
+	    if (matcherObject.find( )) {
+	    	myConfObject.m_Port = matcherObject.group(0);
+	         
+	      } else {
+	         System.err.println("REGEX CANT FIND Configuration");
+	      }
+		
+		
 	}
 }
