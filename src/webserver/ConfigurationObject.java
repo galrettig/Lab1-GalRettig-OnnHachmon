91149460a2fe842f2283f22abae723e6df4c2702
onncho <onncho@Onns-MacBook-Pro.local>
2015-12-06 18:04:07 +0200

    conf file

--- src/webserver/ConfigurationObject.java
+++ src/webserver/ConfigurationObject.java
@@ -1,13 +1,23 @@
 package webserver;
 
+import java.util.HashMap;
+
 public class ConfigurationObject {
 
 	String m_Port;
 	String m_rootFolder;
-	String m_defualtPage;
+	String m_defaultPage;
 	String m_MaxThreads;
+	String m_defaultPageFullUrl;
+	
 
-	public ConfigurationObject() {
-
+	public ConfigurationObject(HashMap<String, String> i_confList) {
+		m_Port = i_confList.get("port");
+		m_rootFolder = i_confList.get("root");
+		m_defaultPage = i_confList.get("defaultPage");
+		m_MaxThreads = i_confList.get("maxThreads");
+		m_defaultPageFullUrl = m_rootFolder + m_defaultPage;
 	}
+	
+	
 }
--- src/webserver/WebServer.java
+++ src/webserver/WebServer.java
@@ -1,70 +1,78 @@
 package webserver;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.HashMap;
+import java.util.Properties;
 
 public class WebServer {
+	
+	
 
 	public static void main(String[] args) throws IOException
 	{
-		// @TODO: open the file and read the data to conf object
-		readFile();
+		readConfFile();
 	}
-	
-	
+
+	//TODO: delete
 	public static void readFile()
 	{
 		Path file = FileSystems.getDefault().getPath("/Users/onncho/Dropbox/Study/IDC/Semester 5/תקשורת/lab1/Lab1-GalRettig-OnnHachmon/configuration", "myWebConf.ini");
 		StringBuilder fileData = new StringBuilder("");
 		try (InputStream in = Files.newInputStream(file);
-		    BufferedReader reader =
-		      new BufferedReader(new InputStreamReader(in))) {
-		    String line = null;
-		    while ((line = reader.readLine()) != null) {
-		        //System.out.println(line);
-		    	fileData.append("\n");
-		        fileData.append(line);
-		    }
+				BufferedReader reader =
+						new BufferedReader(new InputStreamReader(in))) {
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				//System.out.println(line);
+				fileData.append("\n");
+				fileData.append(line);
+			}
 		} catch (IOException e) {
-		    System.err.println(e);
+			System.err.println(e);
 		}
-		
-		fillConfigurationObject(fileData);
-
 	}
 
 
-	private static void fillConfigurationObject(StringBuilder i_StringFromConfFile) {
-		// TODO Auto-generated method stub
-		System.out.println(i_StringFromConfFile);
-		String portRegex = "port=[' ']*([0-9]+)";
-		String rootRegex = "root=[' ']*(.*)";
-		String defaultPageRegex = "defaultPage=[' ']*(.*)";
-		String maxThreadsRegex = "maxThreads=[' ']*(.*)";
-		
-		// Construct Conf Object
-		ConfigurationObject myConfObject = new ConfigurationObject();
-		
-		// Create a Pattern object
-	    Pattern patternObject = Pattern.compile(portRegex);
-	    
-	    // Now create matcher object.
-	    Matcher matcherObject = patternObject.matcher(i_StringFromConfFile);
-		
-	    if (matcherObject.find( )) {
-	    	myConfObject.m_Port = matcherObject.group(0);
-	         
-	      } else {
-	         System.err.println("REGEX CANT FIND Configuration");
-	      }
-		
-		
+	public static void readConfFile()
+	{
+		Properties prop = new Properties();
+		InputStream input = null;
+		HashMap<String, String> confList = new HashMap<>(); 
+
+		try {
+			input = new FileInputStream("/Users/onncho/Dropbox/Study/IDC/Semester 5/תקשורת/lab1/Lab1-GalRettig-OnnHachmon/configuration/myWebconf.ini");
+
+			// load a properties file
+			prop.load(input);
+
+			// get the property value and store them in hashmap
+			confList.put("port", prop.getProperty("port"));
+			confList.put("root", prop.getProperty("root"));
+			confList.put("maxThreads", prop.getProperty("maxThreads"));
+			confList.put("defaultPage", prop.getProperty("defaultPage"));
+			
+			// create conf object
+			ConfigurationObject m_appConfigObject = new ConfigurationObject(confList);
+			
+			//
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
