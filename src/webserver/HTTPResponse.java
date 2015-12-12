a2af9e3707a2a314f7731c9cda79d10a864a0d1b
onncho <onncho@Onns-MacBook-Pro.local>
2015-12-12 21:56:36 +0200

    response3
    s

--- src/webserver/HTTPResponse.java
+++ src/webserver/HTTPResponse.java
@@ -3,86 +3,190 @@ import java.io.File;
 import java.util.HashMap;
 
 public class HTTPResponse {
-	
-	String m_HttpVersion;
-	String m_ContentType; // text or image
+
+	String m_HttpVersion; // client error
 	String m_ContentExtension;
 	String m_RequestedPage; // TODO: check if valid exist
-	int m_ContentLength; // TODO: define max content length in parser class
-	String m_ReferrerHeader;
-	String m_UserAgent;
+	String m_RequestType;
+	HTTPResponseCode m_ErrorsFoundInRequest;
+
 	HashMap<String, String> m_HttpRequestParams;
-	
+
 	String m_Response;
-	String v_ContentType = "content-type: ";
+	HTTPResponseCode m_responseStatusCode;
+	int m_ContentLength; 
+	String m_ContentType;
+	String v_ContentType = "Content-Type: ";
 	String v_ContentLength = "Content-Length: "; 
-	
-	public HTTPResponse(HashMap<String, String> i_HttpRequest) {
-		m_Response = null;
-	}
-	
-	
-	public String constructResponse() {
-		
-		boolean responseDefualt = false;
-		
-		if (!checkResource(m_RequestedPage)) {
-			responseDefualt = true;
-		}
-		
-		m_Response += (m_HttpVersion );
-		m_Response += ("\n" + v_ContentLength + m_ContentLength);
-		
-		// TODO: Print the request Headers with the response
-		
-		return null;
-	}
-	
-	
-	
-	private boolean checkResource(String i_RequestedPage) {
-		
-		// TODO Handle the case windows or mac senarios
-		
-		String pathname = ConfigurationObject.getRoot() + "/" + i_RequestedPage;
-		
-		File file = new File(pathname);
-		
-		if (file.exists())
+
+	public HTTPResponse(HashMap<String, String> i_HttpRequest) 
+	{
+		m_Response = "";
+
+		if (i_HttpRequest.get("erros").equals(HTTPResponseCode.BAD_REQUEST)) 
 		{
-			return true;
+			m_ErrorsFoundInRequest = HTTPResponseCode.BAD_REQUEST;
+			m_responseStatusCode = HTTPResponseCode.BAD_REQUEST;
+			// bla bla bla
 		}
-		
+		else 
+		{
+			m_HttpVersion = i_HttpRequest.get("HTTPVersion");
+
+			// TODO: transfer the extension from the parse
+			m_ContentExtension = i_HttpRequest.get("extension");
+			
+			m_ContentType = constructExtensionToContentType();
+
+			m_RequestedPage = i_HttpRequest.get("URI");
+
+			m_RequestType = i_HttpRequest.get("RequestType");
+		}
+
+	}
+
+	// The Method for clients of this object
+	public String runResponseHandler() 
+	{
+		String result = constructResponse();
+		System.out.println(result);
+		return result;
+	}
+	
+	public 
+	
+	public String constructResponse() 
+	{
+
+		// Construct the first status line
+		m_Response += m_HttpVersion;
+
+		if (m_responseStatusCode.equals(HTTPResponseCode.BAD_REQUEST)) 
+		{
+			m_Response += m_responseStatusCode;
+			return m_Response;
+		}
+		else
+		{
+			constructResponseCode();
+			m_Response += (m_responseStatusCode +
+					"\n" + m_ContentType + "\n" + 
+					v_ContentLength + m_ContentLength);
+		}
+
+		// TODO: Print the request Headers with the response
+
+		return m_Response;
+	}
+
+
+
+	private void constructResponseCode() {
+
+		if (m_RequestType.equals("Other")) 
+		{
+			m_responseStatusCode = HTTPResponseCode.NOT_IMPLEMENTED;
+		}
+		else if (!checkResource(m_RequestedPage)) 
+		{
+			if (!m_responseStatusCode.equals(HTTPResponseCode.INTERNAL_SERVER_ERROR)) {				
+				m_responseStatusCode = HTTPResponseCode.NOT_FOUND;
+			}
+		}
+		else
+		{
+			m_responseStatusCode = HTTPResponseCode.OK;
+		}
+
+	}
+
+
+	private boolean checkResource(String i_RequestedPage) {
+
+		// TODO Handle the case windows or mac senarios
+
+		String pathname = ConfigurationObject.getRoot() + "/" + i_RequestedPage;
+
+		File file = new File(pathname);
+
+		try 
+		{
+			if (file.exists())
+			{
+				// TODO: check if int or long
+				m_ContentLength = (int) file.length();
+				return true;
+			}
+		} 
+		catch (Exception e) {
+			m_responseStatusCode = HTTPResponseCode.INTERNAL_SERVER_ERROR;
+			return false;
+		}
 		return false;
 	}
 
 
+	private String constructExtensionToContentType()
+	{
+		switch (m_ContentExtension) {
+		case "html"	: 
+			m_ContentType = v_ContentType + "text/html";
+			break;
+		case "css"	: 
+			m_ContentType = v_ContentType + "text/css";
+		case "jpg"	: 
+			m_ContentType = v_ContentType + "image/jpeg";
+			break;
+		case "htm"	: 
+			m_ContentType = v_ContentType + "text/html";
+			break;
+		case "jpeg"	: 
+			m_ContentType = v_ContentType + "image/jpeg";
+			break;
+		case "js"	: 
+			m_ContentType = v_ContentType + "application/javascript";
+			break;
+		case "bmp"	: 
+			m_ContentType = v_ContentType + "image/bmp";
+			break;
+		case "gif"	: 
+			m_ContentType = v_ContentType + "image/gif";
+		case "txt"	: 
+			m_ContentType = v_ContentType + "text/plain";
+			break;
+		default		:
+			m_ContentType = v_ContentType + "application/octet-stream";
+			break;
+		}
+		return m_ContentType;
+	}
+
+	/*
 	public String constructContentTypeResponse()
 	{
-		// TODO: check if the request good or not
-		
-		if (m_ContentType == "text") {
-			m_Response += (v_ContentType + "\n" + "text/" + m_ContentType);
-			
+		if (m_ContentType.equals("text")) {
+			m_Response += (v_ContentType + "text/" + m_ContentType);
+
 			// TODO: think of a system how to call the resource after the response was sent
 		}
-		else if (m_ContentType == "image") 
+		else if (m_ContentType.equals("image")) 
 		{
 			String extension = ImageTypes.getExtestion(m_ContentExtension);
-			
+
 			m_Response += (v_ContentType + "\n" + "image/" + extension);
-			
+
 			// TODO: send the image after the request was sent
-			
+
 		} 
 		else 
 		{
 			m_Response += (v_ContentType + "\n" + "application/octet-stream");
-			
+
 			// TODO: send steam to user
 		}
-		
+
 		return m_Response;
 	}
+	*/
 
 }
