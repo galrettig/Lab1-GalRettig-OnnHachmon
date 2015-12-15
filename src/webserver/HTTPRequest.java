b7797572fde313724ed1fb3db9b36ebd66aea59b
galrettig@gmail.com <galrettig@gmail.com>
2015-12-15 20:54:34 +0200

    reDesign for request and parser

--- src/webserver/HTTPRequest.java
+++ src/webserver/HTTPRequest.java
@@ -2,7 +2,7 @@ package webserver;
 import java.util.HashMap;
 
 public class HTTPRequest {
-	
+
 	// GET or POST or ... 
 	HttpRequestType m_RequestType;
 	// Requested Page (/ or /index.html etc.)
@@ -17,13 +17,134 @@ public class HTTPRequest {
 	String m_UserAgent;
 	//Parameters – the parameters in the request (I used java.util.HashMap<String,String> to hold the parameters).
 	String m_HTTPver;
-	HashMap<String, String> m_HttpRequestParams;
+	HashMap<String, String> m_HttpRequestParams = new HashMap<>();
 	HashMap<String, String> m_requestHeaders;
 	String m_originalRequest;
+	//
+	//
+	String m_httpMessageBody;
+	int m_errorCodeIfOccured = -1;//-1 not occured , 4 bad-request , 5 not implemented
+
+
+	public HTTPRequest(String io_originalRequest, String io_httpMessageBody, int contentLength){
+		m_originalRequest = io_originalRequest;
+		m_httpMessageBody = io_httpMessageBody;
+
+		String[] requestAsArray = Parser.SplitRequestToLinesIfAcceptable(m_originalRequest);
+		
+		if(requestAsArray == null){
+			m_errorCodeIfOccured = 4;
+			
+		} else {
+			String[] requestLine = Parser.SplitRequestLineToHeadersIfAcceptable(requestAsArray[0]);
+			
+			if(requestLine == null){
+				m_errorCodeIfOccured = 4;
+				
+			} else {
+				this.m_RequestType = checkIfMethodAcceptable(requestLine[0]);
+				this.m_RequestedPage = requestLine[1];
+				this.m_HTTPver = requestLine[2];
+				
+				if(!HttpRequestType.OTHER.equals(this.m_RequestType)){
+					//can fully parse headers
+					this.handleRequestHeaders(requestAsArray);
+					
+					
+					
+					
+				} else {
+					m_errorCodeIfOccured = 5;
+				}
+			}
+		}
+
+	}
+	
+	
+	private void handleRequestHeaders(String[] request){
+		this.m_requestHeaders = Parser.breakRequestStringToHeaders(request);
+		this.v_IsImage = Parser.checkIfImage(m_RequestedPage);
+		
+		if(this.m_requestHeaders.containsKey("Referer")){
+			this.m_ReferrerHeader = this.m_requestHeaders.get("Referer");
+		}
+		if(this.m_requestHeaders.containsKey("User-Agent")){
+			this.m_ReferrerHeader = this.m_requestHeaders.get("User-Agent");
+		}
+		
+		m_requestHeaders.put("erros", this.m_RequestType);
+		
+		
+		if(!m_RequestedPage.equals("/")){
+			int indexOfExt = m_RequestedPage.indexOf(".");
+			if(indexOfExt > -1){
+				m_requestHeaders.put("extension", m_RequestedPage.substring(indexOfExt + 1));
+			} else {
+				m_requestHeaders.replace("erros", "400 Bad Request");
+			}
+
+		} else {
+			m_requestHeaders.put("extension", "html");
+		}
+		
+		
+		
+	}
+	
+	// check if the request method is acceptable by the server, if not returns -1
+		private HttpRequestType checkIfMethodAcceptable(String requestMethod){
+			HttpRequestType returnValue = HttpRequestType.OTHER;
+			switch (requestMethod) {
+			case "GET":
+				returnValue = HttpRequestType.GET;
+				handleGetRequest();
+				break;
+			case "POST":
+				returnValue = HttpRequestType.POST;
+				handlePostRequest();
+				break;
+			case "HEAD":
+				returnValue = HttpRequestType.HTTP_HEAD;
+				handleHeadRequest();
+				break;
+			case "TRACE":
+				returnValue = HttpRequestType.TRACE;
+				handleTraceRequest();
+				break;
+			default:
+				handleErroredRequest();
+				break;
+			}
+			
+			return returnValue;
+		}
+	
+	private void handleGetRequest(){
+		m_RequestedPage = Parser.parseGetRequest(m_RequestedPage, m_HttpRequestParams);
+	}
+	private void handlePostRequest(){
+		if(this.m_httpMessageBody != null){
+			if(this.m_httpMessageBody.length() > 0){
+				Parser.parsePostRequest(this.m_httpMessageBody, this.m_HttpRequestParams);
+			}
+		}
+		
+	}
+	
+	
+	
+	private void handleHeadRequest(){}
+	private void handleTraceRequest(){}
+	private void handleErroredRequest(){}
+		
+		
+	
+	
 	
 
 	public HTTPRequest(String originalRequest, HashMap<String, String> reqData, HashMap<String,String> params, HttpRequestType type, boolean isImage) {
-		
+
 		m_requestHeaders = reqData;
 		m_originalRequest = originalRequest;
 		this.m_RequestType = type;
@@ -35,18 +156,18 @@ public class HTTPRequest {
 		} else {
 			m_contentLength = 0;
 		}
-		
+
 		if(reqData.containsKey("Referer")){
 			this.m_ReferrerHeader = reqData.get("Referer");
 		}
-		
+
 		if(reqData.containsKey("User-Agent")){
 			this.m_UserAgent = reqData.get("User-Agent"); 
 		}
-		
+
 		v_IsImage = isImage;
 		m_requestHeaders.put("erros", "none");
-		
+
 		if(!m_RequestedPage.equals("/")){
 			int indexOfExt = m_RequestedPage.indexOf(".");
 			if(indexOfExt > -1){
@@ -54,19 +175,19 @@ public class HTTPRequest {
 			} else {
 				m_requestHeaders.replace("erros", "400 Bad Request");
 			}
-			
+
 		} else {
 			m_requestHeaders.put("extension", "html");
 		}
-		
-		
-		
+
+
+
 	}
-	
+
 	public HashMap<String, String> getMap (){
 		return this.m_requestHeaders;
 	}
-	
+
 	public boolean ImplementedMethod(){
 		if(m_RequestType.equals(HttpRequestType.GET) ||
 				m_RequestType.equals(HttpRequestType.POST) || 
@@ -75,18 +196,37 @@ public class HTTPRequest {
 			return true;
 		}
 		return false;
-		
+
 	}
-	
- 
-	
+
+	public HTTPResponseCode mapErrorValueInRequestToResponseType(){
+		HTTPResponseCode code = HTTPResponseCode.OK;
+		switch (this.m_errorCodeIfOccured) {
+		case -1:
+			code = HTTPResponseCode.OK;
+			break;
+		case 4:
+			code = HTTPResponseCode.BAD_REQUEST;
+			break;
+		case 5:
+			code = HTTPResponseCode.NOT_IMPLEMENTED;
+			break;
+
+		default:
+			code = HTTPResponseCode.BAD_REQUEST;
+			break;
+		}
+		return code;
+	}
+
+
 	// webserver -> read configuration file and create object -> open listener (configuration object) -> waiting for request
-	
-			// create http request -> parser class -> create http response -> respond to client
-			
-			// parse request and create request object -> create response
-			
-			
-			
+
+	// create http request -> parser class -> create http response -> respond to client
+
+	// parse request and create request object -> create response
+
+
+
 
 }
--- src/webserver/Parser.java
+++ src/webserver/Parser.java
@@ -1,8 +1,65 @@
 package webserver;
+import java.net.Authenticator.RequestorType;
 import java.util.HashMap;
 
 public class Parser {
 
+	
+	
+	public static void parseRequest(String i_httpRequest){
+		
+		
+		String[] requestAsArray = SplitRequestToLinesIfAcceptable(i_httpRequest);
+		if(requestAsArray != null){
+			String[] requestLine = SplitRequestLineToHeadersIfAcceptable(requestAsArray[0]);
+			if(requestLine != null){
+				HttpRequestType methodType = checkIfMethodAcceptable(requestLine[0]);
+			}
+		}
+		
+		
+		/*
+		HashMap<String, String> params = new HashMap<>();
+		boolean isImage = false;
+		
+		String[] requestSplitted = i_httpRequest.split("\n");
+		String[] requestLine = requestSplitted[0].split(" ");
+		HashMap<String, String> requestHeaders = breakRequestStringToHeaders(requestSplitted);
+		HttpRequestType type = HttpRequestType.OTHER;
+		
+		if(verifyValidRequestLine(requestLine, requestHeaders)){
+			String requestType = requestLine[0];
+			if(requestType.equals(HttpRequestType.GET)){
+				requestLine[1] = parseGetRequest(requestLine[1], params);
+				type = HttpRequestType.GET;
+				isImage = checkIfImage(requestLine[1]);
+			} 
+			else if(requestType.equals(HttpRequestType.POST)){
+				parsePostRequest(msgBody, params);
+				type = HttpRequestType.POST;
+				isImage = checkIfImage(requestLine[1]);
+				
+			}
+			else if(requestType.equals(HttpRequestType.HTTP_HEAD)){
+				requestLine[1] = parseGetRequest(requestLine[1], params);
+				type = HttpRequestType.HTTP_HEAD;
+				isImage = checkIfImage(requestLine[1]);
+				
+			}
+			else if(requestType.equals(HttpRequestType.TRACE)){
+				type = HttpRequestType.TRACE;
+			}
+			//else{}
+			
+			//reqObj = new HTTPRequest(i_httpRequest,requestHeaders, params, type, isImage);
+		}
+		*/
+		
+		
+		
+		
+
+	}
 	
 
 	public static HTTPRequest parseHttp(String i_httpRequest, String msgBody){
@@ -53,7 +110,7 @@ public class Parser {
 	}
 	
 	
-	private static boolean checkIfImage(String URI){
+	static boolean checkIfImage(String URI){
 		String[] acceptedExtensions = {".bmp", ".jpg", ".gif", ".png"};
 		int minExtLength = 4;
 		boolean imageFound = false;
@@ -68,6 +125,59 @@ public class Parser {
 			}
 		}
 		return imageFound;
+	}
+
+	
+	
+	// check if the the request-line is acceptable , returns the the headers of the request-line splitted to spaces into array
+	// or null if it cannot
+	static String[] SplitRequestLineToHeadersIfAcceptable(String i_requestLine){
+		String[] requestLineHeaders = null;
+		if(i_requestLine.indexOf(" ") > -1){
+			String[] requestLineHeadersTemp = i_requestLine.split(" ");
+			if(requestLineHeadersTemp.length == 3){
+				requestLineHeaders = requestLineHeadersTemp;
+			}
+		}
+		return requestLineHeaders;
+	}
+	
+	
+	// check if the the request is split able and returns the the request splitted to lines in array
+	// or null if it cannot
+	static String[] SplitRequestToLinesIfAcceptable(String originalMessage){
+		String[] requestSplitted = null;
+		if(originalMessage.indexOf("\n") > -1){
+			requestSplitted = originalMessage.split("\n");
+		}
+		return requestSplitted;
+	}
+	
+	
+	
+	
+	
+	// check if the request method is accpeptable by the server, if not returns -1
+	static HttpRequestType checkIfMethodAcceptable(String requestMethod){
+		HttpRequestType returnValue = HttpRequestType.OTHER;
+		switch (requestMethod) {
+		case "GET":
+			returnValue = HttpRequestType.GET;
+			break;
+		case "POST":
+			returnValue = HttpRequestType.POST;
+			break;
+		case "HEAD":
+			returnValue = HttpRequestType.HTTP_HEAD;
+			break;
+		case "TRACE":
+			returnValue = HttpRequestType.TRACE;
+			break;
+		default:
+			break;
+		}
+		
+		return returnValue;
 	}
 	
 	private static boolean verifyValidRequestLine(String[] reqLine, HashMap<String, String> headersMap){
@@ -85,7 +195,11 @@ public class Parser {
 	}
 	
 	
-	private static HashMap<String, String> breakRequestStringToHeaders(String[] requestHeaders){
+	
+	
+	
+
+	static HashMap<String, String> breakRequestStringToHeaders(String[] requestHeaders){
 		HashMap<String, String> headers = new HashMap<>();
 		
 		for(int i = 1; i < requestHeaders.length; i++){
@@ -102,9 +216,7 @@ public class Parser {
 	}
 
 	
-
-	
-	private static void parsePostRequest(String messageBody, HashMap<String, String> params) {
+	static void parsePostRequest(String messageBody, HashMap<String, String> params) {
 		if(messageBody.length() == 0){
 			handleRequestErrors();
 			params = null;
@@ -117,7 +229,7 @@ public class Parser {
 	}
 	
 	
-	private static String parseGetRequest(String i_URI, HashMap<String, String> params) {
+	static String parseGetRequest(String i_URI, HashMap<String, String> params) {
 		String parsedURI = "";
 		int indexOfSeperator = i_URI.indexOf("?");
 		if(indexOfSeperator > -1){
@@ -141,7 +253,7 @@ public class Parser {
 	}
 	
 	private static String[] breakEncodedParamsToTuples(String paramsEncoded){
-		String[] tuples;
+		String[] tuples = null;
 		int paramsTupleSeperator = paramsEncoded.indexOf("&");
 		if(paramsTupleSeperator > -1){
 			tuples = paramsEncoded.split("&");
